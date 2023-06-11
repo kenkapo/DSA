a84bf7766ca11f7f76c37b809990c2601ddebf3d
kenkapo <58553879+kenkapo@users.noreply.github.com>
2023-06-11 10:51:17 +0530

    Update maximum.java

--- segment trees/maximum.java
+++ segment trees/maximum.java
@@ -20,7 +20,7 @@ class Solution
 			return seg[i];
 		}
 		else if (low>r || high<l){return Integer.MIN_VALUE;}// query does not contain the range
-		else// query overlaps with the range
+		else// query partially overlaps with the range
 		{
 			int mid=(low+high)/2;
 			int left=queries(2*i+1,low,mid,l,r);
